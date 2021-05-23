0d9ef6dd72563d44772de97932af75a3f178e693
Aleksandr Maksimov <sanyamaks14@gmail.com>
2021-05-24 03:54:23 +0400

    Add Footer Component

---
+++ src/components/main/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = (): JSX.Element => {
+    return (
+        <footer className="footer">
+            <p className="footer__text">C ЛЮБОВЬЮ ОТ MERCURY DEVELOPMENT</p>
+        </footer>
+    )
+}
+
+export default React.memo(Footer)
